f56e6cf759174a42caef7d8fa38a355b0c672e25
JoaoVitorLeite <joaovitorleite@id.uff.br>
2023-09-15 10:38:03 -0300

    LID

--- expt.bash
+++ expt.bash
@@ -33,8 +33,6 @@ mkdir $outputPath
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_PROCESS_TIME:BOOL=ON -DENABLE_SRAND:BOOL=OFF -DENABLE_TESTS:BOOL=OFF -DCMAKE_C_FLAGS_DEBUG="-g -O3" -DCMAKE_CXX_FLAGS_DEBUG="-g -O3" .. -DSOURCE_OUTPUT_PATH:STRING=$outputPath
 make
 
-#./gervLibTest -DATASET_NAME CITIES -INDEX VPTREE -DATASET_TRAIN ../data/cities_norm.csv -DATASET_TRAIN_SEPARATOR , -DATASET_TEST ../data/cities_norm.csv -DATASET_TEST_SEPARATOR , -DISTANCE_FUNCTION EUCLIDEAN -PIVOT_TYPE RANDOM -PIVOT_SAMPLE_SIZE 1.0 -NUM_PIVOTS 2 -SEED 14 -K_MAX 100
-
 for((i=0; i<2; i++));
 do
     nohup ./gervLibTest -INDEX ${index} -DATASET_NAME ${datasetName} -DATASET_TRAIN ${datasetTrain} -DATASET_TRAIN_SEPARATOR ${datasetTrainSeparator} -DATASET_TEST ${datasetTest} -DATASET_TEST_SEPARATOR ${datasetTestSeparator} -DISTANCE_FUNCTION ${distanceFunction} -PIVOT_TYPE "${pivotType[$i]}" -PIVOT_SAMPLE_SIZE "${sampleSize[$i]}" -NUM_PIVOTS ${numPivots} -SEED "${seed[$i]}" -K_MAX ${kMax} -NUM_PER_LEAF ${numPerLeaf} -NUM_BINS ${numBins} -PAGE_SIZE ${pageSize} -NUM_QUERIES_PER_FILE ${numQueryPerFile} -STORE_PIVOT_LEAF ${storePivotLeaf} -STORE_DIRECTORY_NODE ${storeDirectoryNode} -STORE_LEAF_NODE ${storeLeafNode} -USE_LAESA ${useLAESA} &
---
+++ src/main_lid.cpp
@@ -0,0 +1,139 @@
+//
+// Created by joaoleite on 9/15/23.
+//
+
+#include "argparse.hpp"
+#include "Dataset.h"
+#include "Pivot.h"
+#include "Pivots.h"
+#include "EuclideanDistance.h"
+#include "Indexes.h"
+#include "DatasetUtils.h"
+#include "DistanceFactory.h"
+
+using namespace gervLib::dataset;
+using namespace gervLib::pivots;
+using namespace gervLib::distance;
+using namespace gervLib::index;
+
+int main(int argc, char **argv)
+{
+
+    argparse::ArgumentParser program("main_lid");
+
+    program.add_argument("-DATASET")
+        .help("Path to the dataset file")
+        .required();
+
+    program.add_argument("-DATASET_SEPARATOR")
+        .help("Separator used in the dataset file")
+        .required();
+
+    program.add_argument("-DISTANCE_FUNCTION")
+        .help("Distance function to be used")
+        .default_value("EUCLIDEAN");
+
+    program.add_argument("-PIVOT_SAMPLE_SIZE")
+        .help("Number of objects to be used in the pivot sample")
+        .required();
+
+    program.add_argument("-NUM_PIVOTS")
+        .help("Number of pivots to be used")
+        .required();
+
+    program.add_argument("-SEED")
+        .help("Seed for random number generation")
+        .default_value("42");
+
+    program.add_argument("-K_MAX")
+        .help("Maximum number of neighbors to be used in the LID calculation")
+        .default_value("100");
+
+    program.add_argument("-NUM_PER_LEAF")
+        .help("Number of objects per leaf");
+
+    program.add_argument("-PAGE_SIZE")
+        .help("Page size for the index")
+        .default_value("0");
+
+    program.add_argument("-STORE_PIVOT_LEAF")
+        .help("Store the pivot in leaf")
+        .default_value(true)
+        .implicit_value(true);
+
+    program.add_argument("-STORE_DIRECTORY_NODE")
+        .help("Store the directory node")
+        .default_value(false)
+        .implicit_value(false);
+
+    program.add_argument("-STORE_LEAF_NODE")
+        .help("Store the leaf node")
+        .default_value(false)
+        .implicit_value(false);
+
+    program.add_argument("-USE_LAESA")
+        .help("Use LAESA in leaf")
+        .default_value(false)
+        .implicit_value(false);
+
+    program.add_argument("-FILE_NAME")
+        .help("Name of the file to be used in the output")
+        .required();
+
+    program.add_argument("-OUTPUT_PATH")
+        .help("Path to the output directory")
+        .required();
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    std::unique_ptr<Dataset<size_t, double>> data1 = std::make_unique<Dataset<size_t, double>>(program.get<std::string>("-DATASET"), program.get<std::string>("-DATASET_SEPARATOR")),
+            data2 = std::make_unique<Dataset<size_t, double>>(program.get<std::string>("-DATASET"), program.get<std::string>("-DATASET_SEPARATOR"));
+
+    std::unique_ptr<DistanceFunction<BasicArrayObject<size_t, double>>> distance_function;
+    std::variant<size_t, double> pivot_sample_size;
+
+    if(program.get<std::string>("-DISTANCE_FUNCTION") == "EUCLIDEAN")
+    {
+
+        distance_function = std::make_unique<EuclideanDistance<BasicArrayObject<size_t, double>>>();
+
+    }
+    else
+    {
+
+        throw std::invalid_argument("Invalid distance function");
+
+    }
+
+    std::unique_ptr<Pivot<size_t, double>> pivot = std::make_unique<MaxVariancePivots<size_t, double>>();
+    pivot->setSampleSize(std::stod(program.get<std::string>("-PIVOT_SAMPLE_SIZE")));
+    pivot->setSeed(std::stoul(program.get<std::string>("-SEED")));
+
+    std::unique_ptr<Index<size_t, double>> index = std::make_unique<gervLib::index::vptree::VPTree<size_t, double>>(std::move(data1), std::move(distance_function), std::move(pivot),
+            std::stoul(program.get<std::string>("-NUM_PIVOTS")), std::stoul(program.get<std::string>("-NUM_PER_LEAF")),
+            std::stoul(program.get<std::string>("-NUM_PER_LEAF")), program.get<bool>("-STORE_PIVOT_LEAF"),
+            program.get<bool>("-STORE_DIRECTORY_NODE"), program.get<bool>("-STORE_LEAF_NODE"), program.get<bool>("-USE_LAESA"));
+
+    std::unique_ptr<DistanceFunction<BasicArrayObject<size_t, double>>> df = gervLib::distance::DistanceFactory<BasicArrayObject<size_t, double>>::createDistanceFunction(index->getDistanceFunction()->getDistanceType());
+
+    splitByLID(data2, df, index, std::stoul(program.get<std::string>("-K_MAX")), program.get<std::string>("-FILE_NAME"), program.get<std::string>("-OUTPUT_PATH"));
+
+    std::filesystem::path path(program.get<std::string>("-OUTPUT_PATH"));
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        std::filesystem::path file_name = path / (program.get<std::string>("-FILE_NAME") + "_Q" + std::to_string(i+1) + ".csv");
+        splitTrainTest<size_t, double>(file_name, ",", 0.9, std::stoul(program.get<std::string>("-SEED")));
+
+    }
+
+    return 0;
+}
